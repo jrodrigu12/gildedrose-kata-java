222e17a85f6834fd5611d6bb4fc26e6fcd1a1f0c
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-08-10 22:29:55 +0200

    Create golden master unit test from text test fixture

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,10 +1,22 @@
 package com.gildedrose;
 
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static java.lang.Integer.parseInt;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TexttestFixture {
     public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+        simulate(args, System.out);
+    }
 
-        Item[] items = new Item[] {
+    private static void simulate(String[] args, PrintStream out) {
+        out.println("OMGHAI!");
+
+        Item[] items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
@@ -14,24 +26,56 @@ public class TexttestFixture {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new Item("Conjured Mana Cake", 3, 6)};
 
         GildedRose app = new GildedRose(items);
 
         int days = 2;
         if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
+            days = parseInt(args[0]) + 1;
         }
 
         for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
+            out.println("-------- day " + i + " --------");
+            out.println("name, sellIn, quality");
             for (Item item : items) {
-                System.out.println(item);
+                out.println(item);
             }
-            System.out.println();
+            out.println();
             app.updateQuality();
         }
     }
 
+    @Test
+    public void twoDaysSimulation() throws Exception {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        simulate(new String[0], new PrintStream(out));
+        final String output = out.toString("UTF-8");
+
+        assertThat(output).isEqualTo("OMGHAI!\n" +
+                "-------- day 0 --------\n" +
+                "name, sellIn, quality\n" +
+                "+5 Dexterity Vest, 10, 20\n" +
+                "Aged Brie, 2, 0\n" +
+                "Elixir of the Mongoose, 5, 7\n" +
+                "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+                "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 15, 20\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 10, 49\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 5, 49\n" +
+                "Conjured Mana Cake, 3, 6\n" +
+                "\n" +
+                "-------- day 1 --------\n" +
+                "name, sellIn, quality\n" +
+                "+5 Dexterity Vest, 9, 19\n" +
+                "Aged Brie, 1, 1\n" +
+                "Elixir of the Mongoose, 4, 6\n" +
+                "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+                "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 14, 21\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 9, 50\n" +
+                "Backstage passes to a TAFKAL80ETC concert, 4, 50\n" +
+                "Conjured Mana Cake, 2, 5\n" +
+                "\n");
+    }
 }

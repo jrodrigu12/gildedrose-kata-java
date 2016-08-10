7a7674e247925cd1755319f6dbeeb531602c9bdd
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-08-10 22:35:55 +0200

    Reformat test code

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,9 +8,11 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertThat(app.items[0].name).as("name").isEqualTo("foo");
         assertThat(app.items[0].sellIn).as("sellIn").isEqualTo(-1);
         assertThat(app.items[0].quality).as("quality").isEqualTo(0);

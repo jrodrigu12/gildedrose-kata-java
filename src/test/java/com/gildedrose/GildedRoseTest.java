7562f7049948550ce47693df3a69a37a3c2a301d
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-08-10 22:35:13 +0200

    Check all item properties in gilded rose unit test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,9 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertThat(app.items[0].name).isEqualTo("foo");
+        assertThat(app.items[0].name).as("name").isEqualTo("foo");
+        assertThat(app.items[0].sellIn).as("sellIn").isEqualTo(-1);
+        assertThat(app.items[0].quality).as("quality").isEqualTo(0);
     }
 
 }
